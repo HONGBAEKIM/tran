e2ddd6d140d2e1e61971e5d239b7861892fe14eb
Hongbae Kim <hongbaki@c4c4c3.42berlin.de>
2024-06-22 18:46:50 +0200

    save

--- Makefile
+++ Makefile
@@ -1,26 +1,56 @@
-COMPOSE		= ./srcs/docker-compose.yml
+# -----------\ Name \--------------------------------------------------------- #
 
-COMPOSE_CMD = docker-compose -f ${COMPOSE}
+NAME	:= transcendance
 
+# -----------\ Files \-------------------------------------------------------- #
+
+ENV_FILE = --env-file srcs/.env
+COMPOSE = ./srcs/docker-compose.yml
+COMPOSE_CMD = docker compose -f ${COMPOSE} ${ENV_FILE}
+# -----------\ Directories \-------------------------------------------------- #
+
+DATABASE_DIR := M_database
+DJANGO_DIR := M_django-data
+FRONTEND_DIR := M_frontend
+#DIST_DIR := M_dist
+
+
+# -----------\ Rules \-------------------------------------------------------- #
+
+all: $(NAME)
+
+$(NAME):	
+	mkdir -p $(DATABASE_DIR)
+	mkdir -p $(DJANGO_DIR)
+	mkdir -p $(FRONTEND_DIR)
+#	mkdir -p $(DIST_DIR)
+	@${COMPOSE_CMD} up -d
+	
 build:
-	@${COMPOSE_CMD} up --build
-
-up:
-	@${COMPOSE_CMD} up
+	@${COMPOSE_CMD} up --build -d
 
 down:
-	@${COMPOSE_CMD} down
+	@${COMPOSE_CMD} down -d
 
-re: down
-	@${COMPOSE_CMD} up --build
+clean:
+	@echo "clean"
+	-docker ps -qa | xargs docker stop
+	-docker ps -qa | xargs docker rm
+	-docker image ls -qa | xargs docker rmi -f
+	-docker volume ls -q | xargs docker volume rm
+	-docker network ls -q | xargs
+	-docker network ls -q | xargs docker network rm 2>/dev/null
 
-clean: down
-	@docker system prune -a
+fclean: clean
+	-chmod 777 $(DJANGO_DIR)
+	-rm -rf $(DJANGO_DIR)
+	docker run -it --rm -v ./M_database:/delete debian:latest bash -c "rm -rf /delete/*"
+	@$(MAKE) clean
+	-chmod 777 $(DATABASE_DIR)
+	-rm -rf $(DATABASE_DIR)
+	docker system prune -f
 
-fclean: down clean
-	@docker stop $$(docker ps -qa)
-	@docker system prune --all --force --volumes
-	@docker network prune --force
-	@docker volume prune --force
+re: fclean all
 
-.PHONY: build up down re clean fclean
+.PHONY: all build down re clean fclean
+
--- srcs/requirements/nginx/tools/docker-entrypoint.sh
+++ srcs/requirements/nginx/tools/docker-entrypoint.sh
@@ -1,7 +1,7 @@
-#!/bin/bash
+# #!/bin/bash
 
-#Ensures the script exits immediately 
-#if any command returns a non-zero status.
+# #Ensures the script exits immediately 
+# #if any command returns a non-zero status.
 set -e
 
 #Redirects Nginx logs to standard output and standard error. 
@@ -11,16 +11,34 @@ set -e
 ln -sf /dev/stdout /var/log/nginx/access.log && \
 ln -sf /dev/stderr /var/log/nginx/error.log
 
-#Creates a self-signed SSL certificate for Nginx to use for HTTPS.
-mkdir -p /etc/nginx/ssl
-openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
-        -keyout /etc/nginx/ssl/nginx.key \
-        -out /etc/nginx/ssl/nginx.crt \
-        -subj "/C=DE/L=berlin/O=berlin/CN=nginx" \
-        > /dev/null 2>&1
-#silences all output from a command, both normal messages and error messages
+# # Function to generate a self-signed SSL certificate if not already present
+# generate_ssl_cert() {
+#     local cert_file="/etc/nginx/ssl/nginx.crt"
+#     local key_file="/etc/nginx/ssl/nginx.key"
 
+#     # Check if the certificate files already exist
+#     if [ ! -f "$cert_file" ] || [ ! -f "$key_file" ]; then
+#         echo "Generating self-signed SSL certificate..."
 
-# Execute nginx
-#This is necessary to start the Nginx server and keep the container running
+#         mkdir -p /etc/nginx/ssl
+#         openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
+#             -keyout "$key_file" \
+#             -out "$cert_file" \
+#             -subj "/C=DE/L=Berlin/O=Berlin/CN=nginx" \
+#             > /dev/null 2>&1
+#         # Silence all output from the openssl command
+
+#         echo "Self-signed SSL certificate generated."
+#     fi
+
+#     # Copy the certificate to the CA certificates directory
+#     cp "$cert_file" /usr/local/share/ca-certificates/nginx.crt
+#     update-ca-certificates --fresh
+# }
+
+# # Call the function to generate the SSL certificate
+# generate_ssl_cert
+
+# # Execute nginx
+# #This is necessary to start the Nginx server and keep the container running
 exec nginx -g "daemon off;"
