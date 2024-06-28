6364ab8b5cd7a48e41add2c44b64c484917af072
Hongbae Kim <hongbaki@c4c3c3.42berlin.de>
2024-06-28 20:38:37 +0200

    c

--- index.js
+++ index.js
@@ -1,21 +1,14 @@
-// const express = require('express');
-// const app = express();
-// const port = 8080;
-
-// app.get('/', (req, res) => {
-//   res.send('Hello World!');
-// });
-
-// app.listen(port, () => {
-//   console.log(`App listening at http://localhost:${port}`);
-// });
-
 const express = require('express');
 const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+// Define a route handler for the root path
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+// Start the server
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server is running on port ${PORT}`);
 });
-
